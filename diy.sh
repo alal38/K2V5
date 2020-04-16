8a31d30e1c265dc9d4c8c9b6b42af549bb790b0a
alal38 <63663181+alal38@users.noreply.github.com>
2020-04-16 23:10:41 +0800

    Initial commit

---
+++ diy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
